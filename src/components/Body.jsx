29ba7c9fb9852491a5cf6ff8f16e595c04a26169
sachinjadhav <sachinjdv7@gmail.com>
2024-09-19 23:18:09 +0530

    completed lest get hoooked episode

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,11 +1,26 @@
 import resObj from '../utils/mockData';
+import { useState } from 'react';
 import { RestaurantCards } from '../components';
+4;
 const Body = () => {
+  const [restaurantList, setRestaurantList] = useState(resObj);
   return (
     <div className="body">
-      <div className="search">search</div>
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredList = restaurantList.filter(
+              (res) => res.info.avgRating > 4.5
+            );
+            setRestaurantList(filteredList);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
       <div className="res-container">
-        {resObj.map((res) => (
+        {restaurantList.map((res) => (
           <RestaurantCards key={res.info.id} resList={res} />
         ))}
       </div>
